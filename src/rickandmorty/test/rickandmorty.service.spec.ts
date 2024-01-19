7549388570f0a6ad81b7ba15e874876f468fad91
hgiudatto <hgiudatto@prismamp.com>
2024-01-19 11:25:08 -0300

    mock httpservice

--- src/rickandmorty/test/rickandmorty.service.spec.ts
+++ src/rickandmorty/test/rickandmorty.service.spec.ts
@@ -1,21 +1,23 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable prettier/prettier */
-import { HttpModule } from '@nestjs/axios'
+import { createMock, DeepMocked } from '@golevelup/ts-jest'
+import { HttpService } from '@nestjs/axios'
 import { Test, TestingModule } from '@nestjs/testing'
 import { RickandmortyService } from '../rickandmorty.service'
-import { BadRequestException } from '@nestjs/common'
-import { ReadRickandmorty } from '../dto/read-rickandmorty.dto'
 
 describe('RickandmortyService', () => {
   let rickandMortyService: RickandmortyService
+  let httpService: DeepMocked<HttpService>
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      imports: [HttpModule],
       providers: [RickandmortyService],
-    }).compile()
+    })
+      .useMocker(createMock)
+      .compile()
 
     rickandMortyService = module.get<RickandmortyService>(RickandmortyService)
+    httpService = module.get(HttpService)
   })
 
   it('should be defined', () => {
@@ -35,28 +37,45 @@ describe('RickandmortyService', () => {
     })
     it('valid rickandmorty ID to return the rickandmorty name', async () => {
       const goodRicks = ['19']
-      /* const antennaRick = {
-        id: '19',
-        name: 'Antenna Rick',
-        status: 'unknown',
-        species: 'Human',
-        type: 'Human with antennae',
-        gender: 'Male',
-        origin: {
-          name: 'unknown',
-          url: '',
+
+      httpService.axiosRef.mockResolvedValueOnce({
+        data: {
+          id: '19',
+          name: 'Antenna Rick',
+          status: 'unknown',
+          species: 'Human',
+          type: 'Human with antennae',
+          gender: 'Male',
+          origin: {
+            name: 'unknown',
+            url: '',
+          },
+          location: {
+            name: 'unknown',
+            url: '',
+          },
+          image: 'https://rickandmortyapi.com/api/character/avatar/19.jpeg',
+          episode: ['https://rickandmortyapi.com/api/episode/10'],
+          url: 'https://rickandmortyapi.com/api/character/19',
+          created: '2017-11-04T22:28:13.756Z',
         },
-        location: {
-          name: 'unknown',
-          url: '',
-        },
-        image: 'https://rickandmortyapi.com/api/character/avatar/19.jpeg',
-        episode: ['https://rickandmortyapi.com/api/episode/10'],
-        url: 'https://rickandmortyapi.com/api/character/19',
-        created: '2017-11-04T22:28:13.756Z',
-      } */
+      })
       const ram = rickandMortyService.fetchEveryRickAndMorty(goodRicks)
       await expect(ram).not.toBeNull()
+    })
+
+    it('if RickAndMorty API unexpectedly changes, throw an error', async () => {
+      const goodRicks = ['827']
+      httpService.axiosRef.mockResolvedValueOnce({
+        data: 'Unexpected data',
+        headers: {},
+        config: { url: '' },
+        status: 200,
+        statusText: '',
+      })
+
+      const ram = rickandMortyService.fetchEveryRickAndMorty(goodRicks)
+      await expect(ram).rejects.toBeInstanceOf(TypeError)
     })
   })
 })
